2ac7fd6603075b1d790a3818fc601ab39f94b66b
Ploxbot <96308767+Ploxbot@users.noreply.github.com>
2022-02-08 16:16:42 +0700

    Added Profile Controllers
    for update and rendering

---
+++ controllers/profile.js
@@ -0,0 +1,16 @@
+// Packages
+const express = require('express')
+const router = express.Router()
+
+//PROFILE VIEW CONTROLLER
+router.get('/', (req, res) => {
+  console.log('profile')
+  res.send('profilepage')
+})
+
+//PROFILE UPDATE CONTROLLER
+router.patch('/', (req, res) => {
+  console.log('profile updated')
+  res.send('profile updated')
+})
+module.exports = router
