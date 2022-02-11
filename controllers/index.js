67342fd206383eeafb6f834d0abe01ef3c01a9e6
Ploxbot <96308767+Ploxbot@users.noreply.github.com>
2022-02-11 14:26:59 +0700

    changed controllers
    added rendering

--- controllers/index.js
+++ controllers/index.js
@@ -4,24 +4,27 @@ const router = express.Router()
 
 //LOGIN VIEW CONTROLLER
 router.get('/', (req, res) => {
-  console.log('auth')
-  res.send('login')
+  res.render('login')
 })
 
 //SIGNUP VIEW CONTROLLER
 router.get('/signup', (req, res) => {
-  console.log('signup')
-  res.send('signup')
+  res.render('signup')
+})
+
+//INFO VIEW CONTROLLER
+router.get('/info', (req, res) => {
+  res.render('info')
 })
 
 //LOGIN POST CONTROLLER
-post.get('/', (req, res) => {
+router.post('/', (req, res) => {
   console.log('loggedin')
   res.send('loggedin')
 })
 
 //SIGNUP POST CONTROLLER
-router.get('/signup', (req, res) => {
+router.post('/signup', (req, res) => {
   console.log('signedup')
   res.send('signedup')
 })
