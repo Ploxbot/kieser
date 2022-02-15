ea41cff08b1f0285db71884dbe246bb3c105a77c
thaisfinck <thaisfinck@gmail.com>
2022-02-15 12:51:20 +0700

    feat(index): create signup

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,9 @@
 const express = require('express')
 const router = express.Router()
 
+//Models
+const Users = require('../models/users')
+
 //LOGIN VIEW CONTROLLER
 router.get('/', (req, res) => {
   res.render('login')
@@ -24,9 +27,25 @@ router.post('/', (req, res) => {
 })
 
 //SIGNUP POST CONTROLLER
-router.post('/signup', (req, res) => {
-  console.log('signedup')
-  res.send('signedup')
+router.post('/signup', async (req, res, next) => {
+  try {
+    let existingUser = await Users.findOne({ email: req.body.email })
+    if (existingUser) {
+      throw new Error('User already exist!')
+    } else {
+      let user = await Users.create(req.body)
+      //console.log({user})
+      req.login(user, err => {
+        if (err) {
+          throw err
+        } else {
+          res.redirect('/')
+        }
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
 })
 
 module.exports = router
