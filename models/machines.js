25d7b267d9ca772c9471a2c1b51896bf11e2059b
Ploxbot <96308767+Ploxbot@users.noreply.github.com>
2022-02-15 16:55:45 +0700

    removed comments

--- models/machines.js
+++ models/machines.js
@@ -14,44 +14,48 @@ module.exports = mongoose.model(
       type: String,
       required: true
     },
+    img: {
+      type: String,
+      required: true
+    },
     weight: {
       type: Number,
       required: true
-    }
-    // duration: {
-    // 	type: Date, 			//TIME ???
-    // 	dafault: 0,
-    // 	required: true
-    // },
-    // note: String,
-    // seat: Number,
-    // lean: Number,
-    // feet: Number,
-    // arms1: Number,
-    // arms2: String,
-    // lean1: Number,
-    // lean2: String,
-    // legs1:String,
-    // legs2:[Number, String], //two values
-    // heel: Number,
-    // spann: Number,
-    // ball: Number,
-    // pillow: String,
-    // range: [Number], //two values
-    // hole: Number,
-    // distance: Number,
-    // height: Number,
-    // pad: Number,
-    // handles1: Number,
-    // handles2: String,
-    // Start: Number,
-    // crank: Number,
-    // rom_r: Number,
-    // rom_l: Number,
-    // head: Number,
-    // tower: Number,
-    // belt: String,
-    // rope: String
+    },
+    duration: {
+      type: Date, //TIME ???
+      dafault: 0,
+      required: true
+    },
+    note: String,
+    seat: Number,
+    lean: Number,
+    feet: Number,
+    arms1: Number,
+    arms2: String,
+    lean1: Number,
+    lean2: String,
+    legs1: String,
+    legs2: [Number, String], //two values
+    heel: Number,
+    spann: Number,
+    ball: Number,
+    pillow: String,
+    range: [Number], //two values
+    hole: Number,
+    distance: Number,
+    height: Number,
+    pad: Number,
+    handles1: Number,
+    handles2: String,
+    Start: Number,
+    crank: Number,
+    rom_r: Number,
+    rom_l: Number,
+    head: Number,
+    tower: Number,
+    belt: String,
+    rope: String
   },
   {
     strict: false
