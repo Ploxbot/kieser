cd6dc9f7ddd674acf207c3770e75af33df94779a
thaisfinck <thaisfinck@gmail.com>
2022-02-17 15:59:32 +0700

    feat(machines): timer

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,22 @@
+const counter = document.querySelector("#counter"); 
+const reset = document.querySelector("#reset"); 
+const start = document.querySelector("#start"); 
+const stop = document.querySelector("#stop"); 
+
+counter.innerHTML = 00; 
+let interval; 
+let i = 0; 
+
+start.addEventListener("click", startCounter); 
+stop.addEventListener("click", stopCounter); 
+reset.addEventListener("click", resetCounter); 
+
+function startCounter(){ 
+    interval = setInterval(function() { 
+      counter.innerHTML= i++; 
+    }, 1000); 
+  } 
    
+  function stopCounter(){ 
+    clearInterval(interval);  
+  } 
